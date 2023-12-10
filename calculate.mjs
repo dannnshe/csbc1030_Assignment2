0d4f56266f4ddfb8570849e971e090838bb30ba9
dannnshe <74943644+dannnshe@users.noreply.github.com>
2023-12-09 19:17:52 -0500

    Add files via upload

---
+++ calculate.mjs
@@ -0,0 +1,12 @@
+
+export const calculateArea = (length, width) => {
+    return length*width;
+}
+
+export const calculateVolume = (length, width, height) => {
+    return length*width*height;
+}
+
+export const calculateSquare = (length) => {
+    return length**2;
+}
