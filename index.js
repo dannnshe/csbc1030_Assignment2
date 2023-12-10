34db57201fa867f8226ae0f59318546c683b9155
dannnshe <74943644+dannnshe@users.noreply.github.com>
2023-12-09 19:17:22 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const {calculateArea, calculateSquare, calculateVolume} = require('./calculate')
+
+console.log(`Area of Figure ${calculateArea(4, 2)}`);
+
+console.log(`Volume of Figure ${calculateVolume(2, 2, 2)}`); 
+
+console.log(`Square of Figure ${calculateSquare(4)}`);
